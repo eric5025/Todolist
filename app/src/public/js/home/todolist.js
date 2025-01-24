e6b442881da8f3dcf2be2e666ac97b7d9af0172a
eric5025 <kweon5025@gmail.com>
2025-01-24 11:29:37 +0900

    dom 을 이용하여 html객체 저장

--- app/src/public/js/home/todolist.js
+++ app/src/public/js/home/todolist.js
@@ -1,3 +1,11 @@
 "use strict";
 
-console.log("hello");
+const newlist = document.querySelector("#newlist"),
+  addBtn = document.querySelector("button");
+
+addBtn.addEventListener("click", add);
+function add() {
+  const req = {
+    newlist: newlist.value,
+  };
+}
